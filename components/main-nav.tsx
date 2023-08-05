0df38a3c7e5b9d4dc9f579e14634293840db3b15
Sanjero20 <gelosantos72@gmail.com>
2023-08-05 14:22:19 +0800

    Add navbar

---
+++ components/main-nav.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import NavLink from './ui/NavLink';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
+function MainNav() {
+  return (
+    <header className="h-16 w-full bg-primary text-white">
+      <div className="container flex h-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Fake Store</h1>
+
+        <nav className="flex gap-10">
+          <NavLink href="/">HOME</NavLink>
+          <NavLink href="/shop">SHOP</NavLink>
+          <NavLink href="/about">ABOUT</NavLink>
+        </nav>
+
+        <Link href="/cart">
+          <AiOutlineShoppingCart size={24} />
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default MainNav;
---
+++ components/ui/NavLink.tsx
@@ -0,0 +1,21 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+interface LinkProps {
+  children: React.ReactNode;
+  href: string;
+}
+
+function NavLink({ children, href }: LinkProps) {
+  const pathname = usePathname();
+  const styles = pathname === href ? 'border-b-2' : '';
+
+  return (
+    <Link href={href} className={styles}>
+      {children}
+    </Link>
+  );
+}
+
+export default NavLink;
