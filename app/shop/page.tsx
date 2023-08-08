1fcd19fad6506e6482df518d1bcfac1fa938f48b
Sanjero20 <gelosantos72@gmail.com>
2023-08-08 13:58:39 +0800

    Responsive grid

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -2,7 +2,7 @@ import ProductCard from '@/components/product-card';
 import { Product } from '@/lib/types';
 
 async function getData(): Promise<Product[]> {
-  const response = await fetch('https://fakestoreapi.com/products', {
+  const response = await fetch('https://fakestoreapi.com/products/', {
     cache: 'force-cache',
   });
   return response.json();
@@ -12,7 +12,7 @@ async function ProductsPage() {
   const products = await getData();
 
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
