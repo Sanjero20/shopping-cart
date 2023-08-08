c328fce71540742200eafce445f26df8d7c88bd4
Sanjero20 <gelosantos72@gmail.com>
2023-08-08 09:56:30 +0800

    Display list of products

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,5 +1,23 @@
-function ProductsPage() {
-  return <div>List of products</div>;
+import ProductCard from '@/components/product-card';
+import { Product } from '@/lib/types';
+
+async function getData(): Promise<Product[]> {
+  const response = await fetch('https://fakestoreapi.com/products', {
+    cache: 'force-cache',
+  });
+  return response.json();
+}
+
+async function ProductsPage() {
+  const products = await getData();
+
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
 }
 
 export default ProductsPage;
