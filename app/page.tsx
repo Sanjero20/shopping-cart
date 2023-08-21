021915c90687e42a17b3b5fdc764414e158bdcaf
Sanjero20 <gelosantos72@gmail.com>
2023-08-21 12:42:37 +0800

    Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
-function Home() {
-  return <div className=""></div>;
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
+function HomePage() {
+  return (
+    <div className="flex h-full flex-col items-center">
+      <section className="flex flex-col items-center justify-center gap-4 text-center">
+        <h1 className="text-3xl font-bold md:text-4xl">
+          Enter the World of Unrivaled Shopping
+        </h1>
+        <p className="text-lg text-neutral-600 md:w-2/3 md:text-xl">
+          Immerse yourself in a shopping adventure like no other, where our vast
+          array of products, unbeatable value, and remarkable customer care
+          redefine your entire shopping voyage. Begin exploring today and
+          revolutionize your retail experience with us.
+        </p>
+
+        <Link href={'products'}>
+          <Button className="text-lg">Shop now</Button>
+        </Link>
+      </section>
+    </div>
+  );
 }
 
-export default Home;
+export default HomePage;
